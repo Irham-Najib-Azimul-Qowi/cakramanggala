330eb86b7850163424d3b2470b9b44e8fde08e95
Irham-Najib-Azimul-Qowi <irhamnajibazimulqowi@gmail.com>
2026-07-15 19:37:43 +0700

    debug: increase log viewer output to 400 lines

--- public/view-log.php
+++ public/view-log.php
@@ -3,7 +3,7 @@ header('Content-Type: text/plain');
 $logFile = __DIR__ . '/../storage/logs/laravel.log';
 if (file_exists($logFile)) {
     $lines = file($logFile);
-    $lastLines = array_slice($lines, -100);
+    $lastLines = array_slice($lines, -400);
     echo implode("", $lastLines);
 } else {
     echo "Laravel log file not found at " . $logFile;
