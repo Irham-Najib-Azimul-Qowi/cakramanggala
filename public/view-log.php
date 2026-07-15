d6008672881f844938ebd168ec159717604d26c1
Irham-Najib-Azimul-Qowi <irhamnajibazimulqowi@gmail.com>
2026-07-15 19:36:22 +0700

    debug: add laravel log viewer

---
+++ public/view-log.php
@@ -0,0 +1,10 @@
+<?php
+header('Content-Type: text/plain');
+$logFile = __DIR__ . '/../storage/logs/laravel.log';
+if (file_exists($logFile)) {
+    $lines = file($logFile);
+    $lastLines = array_slice($lines, -100);
+    echo implode("", $lastLines);
+} else {
+    echo "Laravel log file not found at " . $logFile;
+}
